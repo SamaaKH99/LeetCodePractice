cbacf91ec58f86fe44b06408443d9bc8194891a8
Samaa <93490251+SamaaKH99@users.noreply.github.com>
2022-05-13 23:23:53 +0200

    Time: 105 ms (19.90%), Space: 35.7 MB (66.41%) - LeetHub

---
+++ 771-jewels-and-stones/771-jewels-and-stones.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int NumJewelsInStones(string jewels, string stones) {
+        //Declaration
+            int counter = 0;
+            //logic
+            for(int i = 0; i < jewels.Length; i++)
+            {
+                for(int j = 0; j < stones.Length; j++)
+                {
+                    if(jewels[i] == stones[j])
+                    {
+                        counter++;
+                    }
+                }    
+            }
+            return counter;
+    }
+}
