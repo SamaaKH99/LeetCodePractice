60c32812115fd503484091b6e3c63a0d43421e66
Samaa <93490251+SamaaKH99@users.noreply.github.com>
2022-05-17 02:18:31 +0200

    Time: 86 ms (55.98%), Space: 35 MB (88.80%) - LeetHub

---
+++ 28-implement-strstr/28-implement-strstr.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public int StrStr(string haystack, string needle) {
+        // Case
+            if (haystack.Length == 0 || needle.Length == 0)
+                return 0;
+
+            // Logic
+            for(int i = 0; i < (haystack.Length + 1 - needle.Length); i++)
+            {
+                for(int j = 0; j < needle.Length; j++)
+                {
+                    if (haystack[i + j] != needle[j])
+                    {
+                        break;
+                    }
+                    else if (j == needle.Length - 1)
+                        return i;
+                }
+            }
+            return -1;
+    }
+}
