86c42fae76b5d6266b7495cbaa9c7614152a6636
Samaa <93490251+SamaaKH99@users.noreply.github.com>
2022-06-02 23:07:09 +0200

    Time: 112 ms (78.19%), Space: 43 MB (25.73%) - LeetHub

--- 136-single-number/136-single-number.cs
+++ 136-single-number/136-single-number.cs
@@ -1,11 +1,10 @@
 public class Solution {
     public int SingleNumber(int[] nums) {
-        
         int xor = 0;
-foreach (var num in nums)
-{
-xor ^= num;
-}
-return xor;
+            foreach (var num in nums)
+            {
+                xor ^= num;
+            }
+            return xor;
     }
 }
