b2a160e1c5b7bb0d054aadffb161a5d7cbb9920f
Samaa <93490251+SamaaKH99@users.noreply.github.com>
2022-06-03 19:17:30 +0200

    Time: 95 ms (66.55%), Space: 36.7 MB (85.34%) - LeetHub

---
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cs
@@ -0,0 +1,33 @@
+public class Solution {
+    public string ReverseWords(string s) {
+        // Declaration
+            StringBuilder x = new();
+            string[] str = s.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            int start = 0, end = str.Length-1;
+            string tmp = string.Empty;
+            // Case
+            if (s == null)
+                return null;
+            // Logic
+            
+            
+            while(start < end)
+            {
+                tmp = str[start];
+                str[start] = str[end];
+                str[end] = tmp;
+                start++;
+                end--;
+            }
+            
+            for(int i = 0; i < str.Length; i++)
+            {
+                x.Append(str[i]);
+                if(i != str.Length-1)
+                    x.Append(' ');
+            }
+            s = x.ToString();
+            return s;
+
+    }
+}
